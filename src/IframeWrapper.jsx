f98a45a106bd2c8daf30ac3c844bb8d8f6251138
Jason Toubia <jason.toubia@nvisionkc.com>
2024-02-16 15:49:50 -0600

    Trying to get it to register

--- src/IframeWrapper.jsx
+++ src/IframeWrapper.jsx
@@ -5,7 +5,7 @@ import { IFrame } from "./Iframe";
 
 class IFrameWrapper extends reactToWebComponent(IFrame, React, ReactDOM, {
   shadow: "open",
-  props: ["name", "title", "src", "height"],
+  props: ["name", "title", "src", "width", "height"],
 }) {
   static getMetaConfig = () =>
     Promise.resolve({
