abf51de7481bd7c19ce79b14d8a9906456c60e3d
Jason Toubia <jason.toubia@nvisionkc.com>
2024-04-04 09:31:28 -0500

    updates

--- src/Iframe.jsx
+++ src/Iframe.jsx
@@ -1,11 +1,24 @@
 import React, { useEffect } from 'react';
-
-export const IFrame = ({ name = "UploadControl", title = "Upload Control", src = "https://localhost:7242/upload", height = "400px", width = "100%" }) => {
+export const IFrame = ({ name = "UploadControl", title = "Upload Control", src = "https://desktop.tunnel.jasontoubia.net?current=" + current + "&overlap=" + overlap, height = "400px", width = "100%" }) => {
   const styles = `...`; // your styles remain unchanged
 
   const elementStyles = { height: height, borderWidth: "0px", width: width };
 
    // Empty dependency array means this effect runs once on mount
+   useEffect(() => {
+    const handleMessage = (event) => {
+      // Check the origin of the message and ensure it's from your iframe
+      //if (event.origin === "https://basysapphost") {
+        // Handle the message, update form values or perform actions
+      //}
+      console.log(event);
+    };
+  
+    window.addEventListener("message", handleMessage);
+  
+    // Cleanup on component unmount
+    return () => window.removeEventListener("message", handleMessage);
+  }, []); // Empty array ensures this runs once on mount
 
   return (
     <div>
